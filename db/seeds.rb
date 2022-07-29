bde0a036b46716178cd148c933468e989e4df046
Nelson Feng <nelsonfeng98@gmail.com>
2022-07-29 12:26:56 +1000

    debugging delete user function

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.destroy_all
 
-u1 = User.create :email => 'nelson@ga.co', :password =>'nelson', :admin => true
-u2 = User.create :email => 'jono@ga.co',:password =>'nelson', :info => "Nice person"
+u1 = User.create :email => 'nelson@ga.co', :password =>'nelson', :image =>"https://carlsbadcravings.com/wp-content/uploads/2020/07/Doner-Kebabs-v14-500x500.jpg", :info => "However, the end of the world approaches, and time is however, the end of the world approaches, and time is however, the end of the world approaches, and time is" :admin => true
+u2 = User.create :email => 'jono@ga.co',:password =>'nelson',:image =>"https://carlsbadcravings.com/wp-content/uploads/2020/07/Doner-Kebabs-v14-500x500.jpg", :info => "However, the end of the world approaches, and time is however, the end of the world approaches, and time is however, the end of the world approaches, and time is"
 
 puts "#{ User.count } users"
 
